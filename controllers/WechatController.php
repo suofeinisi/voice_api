2b184d6fad14e65bd2f1b2ead8963f1eb619397a
jiaxu <jiaxu_php@163.com>
2018-12-31 10:58:54 +0800

    m

--- controllers/WechatController.php
+++ controllers/WechatController.php
@@ -11,8 +11,19 @@ namespace app\controllers;
 
 class WechatController extends BaseController
 {
+    public function actionSignin()
+    {
+        echo 333;die;
+        $code = \Yii::$app->request->get('code');
+        return 111;die;
+    }
+
     public function actionTest()
     {
+        $code = \Yii::$app->request->get('code');
+        $miniProgram = \Yii::$app->wechat->miniProgram;
+        $response = $miniProgram->auth->session();
+        $userInfo = $miniProgram->access_token;
         if(\Yii::$app->wechat->isWechat &&!\Yii::$app->wechat->isAuthorized){
             return \Yii::$app->wechat->authorizeRequired()->send();
         }
