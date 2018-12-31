3b70de22487c090d768477688137139ac926ba33
jiaxu <jiaxu_php@163.com>
2018-12-31 15:31:26 +0800

    m

--- config/params.php
+++ config/params.php
@@ -2,6 +2,7 @@
 
 return [
     'adminEmail' => 'admin@example.com',
+    'messageCode' => require 'message.php',
     'wechatConfig' => require 'wechat.php',
     'wechatMiniProgramConfig' => require 'mini_program.php',
 ];
